13f0c117397873a9a3fb6179237ff2f149e10c38
larissagrego01 <laligrego@yahoo.com.br>
2015-07-01 18:14:35 -0300

    ATIVIDADES DE POO - 1 AO 8 E OPCIONAL

---
+++ poo-paint/AreaDeDesenho.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace poo_paint
+{
+    partial class AreaDeDesenho
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.comboxFerramenta = new System.Windows.Forms.ComboBox();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(212, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // comboxFerramenta
+            // 
+            this.comboxFerramenta.FormattingEnabled = true;
+            this.comboxFerramenta.Location = new System.Drawing.Point(10, 0);
+            this.comboxFerramenta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboxFerramenta.Name = "comboxFerramenta";
+            this.comboxFerramenta.Size = new System.Drawing.Size(92, 21);
+            this.comboxFerramenta.TabIndex = 1;
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(106, 0);
+            this.btnAbrir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(42, 19);
+            this.btnAbrir.TabIndex = 2;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(152, 0);
+            this.btnSalvar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(49, 19);
+            this.btnSalvar.TabIndex = 3;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // AreaDeDesenho
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(212, 206);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.comboxFerramenta);
+            this.Controls.Add(this.toolStrip1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "AreaDeDesenho";
+            this.Text = "AreaDeDesenho";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Desenha);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.DesenhaCliqueInicial);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.DesenhaMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.DesenhaCliqueFinal);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ComboBox comboxFerramenta;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnSalvar;
+    }
+}
---
+++ poo-paint/Figura.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poo_paint
+{
+    public class Figura
+    {
+        protected int px;
+        protected int py;
+
+        public virtual string Imprime()
+        {
+            return "figura[x:" + px + ",y:" + py + "]";
+        }
+
+        public virtual string GeraLinhaArquivo()
+        {
+            return "Figura(x, y, l, a)";
+        }
+
+        public virtual void Desenha(Graphics g)
+        {
+        }
+    }
+}
